c148dffd782edbd14768d4b189bab04eae681438
K.Stepanov <unholy.vostok@gmail.com>
2016-03-23 17:17:05 +0200

    Added a victory conditions & tests for them

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -20,11 +20,31 @@ module TicTacToe
       false
     end
 
+    private
+
     def draw?
-    grid.flatten.map { |cell| cell.value }.none_empty?
+      grid.flatten.map { |cell| cell.value }.none_empty?
     end
 
-    private
+    def winner
+      winning_positions.each do |winning_positions|
+        next if winning_positions_values(winning_positions).all_empty?
+        return true if winning_positions_values(winning_positions).all_same?
+      end
+    end
+
+    def winning_positions
+      grid
+      grid.transpose
+      diagonals
+    end
+
+    def diagonals
+      [
+        [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2)],
+        [get_cell(0, 2), get_cell(1,1), get_cell(2, 0)]
+      ]
+    end
 
     def default_grid
       Array.new(3) { Array.new(3) { Cell.new } }
